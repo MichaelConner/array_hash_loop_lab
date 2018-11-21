b634a7f06986ba69a31f73778d42eeafcc07634c
Michael Conner <michaelconner@hotmail.co.uk>
2018-11-21 16:31:29 +0000

    example 4 added

--- start_point/my_functions.rb
+++ start_point/my_functions.rb
@@ -1,3 +1,21 @@
 def add_array_lengths(array_1, array_2)
 return array_1.length + array_2.length
 end
+
+
+def sum_array(numbers)
+ for number in numbers
+   total = number.to_i + total.to_i
+ end
+ return total
+end
+
+
+def find_item(array, item)
+  for name in array
+    if name == item
+      return true
+    end
+  end
+  return false
+end
